eaffcff48c2f8b3c8cf3451cb34602688ab9e7ef
aldo512 <aldo.l.ojeda.l>
2019-02-19 16:15:17 +0100

    test

--- Session 5/ex-1.py
+++ Session 5/ex-1.py
@@ -1,17 +1,26 @@
 def count_a(seq):
     """Counting the number of As in the string"""
 
-    result = 0
+    As = 0
+    Cs = 0
+    Ts = 0
+    Gs = 0
     for b in seq:
         if b == 'A':
-            result += 1
+            As += 1
+        if b == 'C':
+            Cs += 1
+        if b == 'T':
+            Ts += 1
+        if b == 'G':
+            Gs += 1
 
-    return result
+    return As, Cs, Ts, Gs
 
 
 # Main program
 
-s = "AGTACACTGGT"
+s = "AGGTACACTGGT"
 na = count_a(s)
 print("The are {} As in the sequence".format(na))
 
@@ -19,4 +28,7 @@ print("The are {} As in the sequence".format(na))
 tl = len(s)
 
 print("This sequence is {} bases in length".format(tl))
-print("The percentages of As is {}%".format(round(100.0 * na/tl, 1)))
+
+bases = 'ACTG'
+for L in range(len(bases)):
+    print('The percentages of', bases[L], 's', 'is {}%'''.format(round(100.0 * na[L] / tl, 1)))
